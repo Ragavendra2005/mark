3aebd03d20ec13887717cc494d83e0470959ac89
Ragavendra2005 <165249128+Ragavendra2005@users.noreply.github.com>
2024-08-06 08:47:07 +0530

    Add files via upload

---
+++ review 2 day 2/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,39 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User loginUser(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public boolean deleteUser(Long id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
